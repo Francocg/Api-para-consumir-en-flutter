00a7468296d7d93bbd5aea77493feafaa03a745e
francocg <francocabanillas@upeu.edu.pe>
2021-11-10 23:09:56 -0500

    ff

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 const cors = require('cors');
 
 app.use(express.json())
+require('dotenv').config();
 
 app.use('/', require('./routes/api'));
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port 3000`);
 });
